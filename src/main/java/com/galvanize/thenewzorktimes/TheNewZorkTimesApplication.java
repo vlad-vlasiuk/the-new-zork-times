846b7a8ff2f166f67826d905328914d6ce9132c7
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-14 10:59:58 -0700

    create spring boot app

---
+++ src/main/java/com/galvanize/thenewzorktimes/TheNewZorkTimesApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.thenewzorktimes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheNewZorkTimesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheNewZorkTimesApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/galvanize/thenewzorktimes/TheNewZorkTimesApplicationTests.java
@@ -0,0 +1,17 @@
+package com.galvanize.thenewzorktimes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheNewZorkTimesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
